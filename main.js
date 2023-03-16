34048ce7df1d7e2a6f84240ccb48a3e0124125d4
Lawe Sosah <lawesosah@Lawes-MacBook-Pro.local>
2023-03-16 09:18:46 +0200

    minor changes

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
-
+import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // Scene
-
+import gsap from "gsap";
 const scene = new THREE.Scene();
 
 // Creating the sphere
@@ -17,12 +18,34 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Sizes
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Params. First is color
+const light = new THREE.PointLight(0xffffff, 1, 100);
+
+// Params are x y and z
+light.position.set(0, 10, 10);
+scene.add(light);
 // Camera
 
-// Parameters: FOV, Next 2 is aspect ratio
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+// Parameters: FOV, Next 2 is aspect ratio, Next is clipping points for close and far
+
+// The sizes object makes the scene span the screen
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 20;
 scene.add(camera);
+
+// scene.position.z = 10;
 
 // Renderer
 
@@ -30,6 +53,58 @@ const canvas = document.querySelector(".webgl");
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 
-renderer.setSize(800, 600);
-
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
 renderer.render(scene, camera);
+
+// Controls
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+
+// Resize
+
+window.addEventListener("resize", () => {
+  (sizes.width = window.innerWidth), (sizes.height = window.innerHeight);
+  camera.updateProjectionMatrix();
+  camera.aspect = sizes.width / sizes.height;
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  // mesh.position.x += 0.2;
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop();
+
+// GSap timelines(for animations)
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+// Mouse animation colour
+
+let mouseDown = false;
+let rgb = [17, 23, 55];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mousedown", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    console.log(rgb);
+  }
+});
